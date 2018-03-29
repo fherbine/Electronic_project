0f8ad1603b88b646d4b4421059ae0e19b989bcc8
LabElec <labelec@Bocals-iMac-4.local>
2018-03-29 15:04:42 +0200

    feat: TP fm

---
+++ TP/MPLABX_vomnes/FirmwareTP1.X.vomnes-/conf_bit.c
@@ -0,0 +1,35 @@
+
+// PIC32MX340F512H Configuration Bit Settings
+
+// 'C' source line config statements
+
+#include <xc.h>
+
+// DEVCFG3
+// USERID = No Setting
+
+// DEVCFG2
+#pragma config FPLLIDIV = DIV_12        // PLL Input Divider (12x Divider)
+#pragma config FPLLMUL = MUL_24         // PLL Multiplier (24x Multiplier)
+#pragma config FPLLODIV = DIV_256       // System PLL Output Clock Divider (PLL Divide by 256)
+
+// DEVCFG1
+#pragma config FNOSC = FRCDIV           // Oscillator Selection Bits (Fast RC Osc w/Div-by-N (FRCDIV))
+#pragma config FSOSCEN = ON             // Secondary Oscillator Enable (Enabled)
+#pragma config IESO = ON                // Internal/External Switch Over (Enabled)
+#pragma config POSCMOD = OFF            // Primary Oscillator Configuration (Primary osc disabled)
+#pragma config OSCIOFNC = ON            // CLKO Output Signal Active on the OSCO Pin (Enabled)
+#pragma config FPBDIV = DIV_8           // Peripheral Clock Divisor (Pb_Clk is Sys_Clk/8)
+#pragma config FCKSM = CSDCMD           // Clock Switching and Monitor Selection (Clock Switch Disable, FSCM Disabled)
+#pragma config WDTPS = PS1048576        // Watchdog Timer Postscaler (1:1048576)
+#pragma config FWDTEN = ON              // Watchdog Timer Enable (WDT Enabled)
+
+// DEVCFG0
+#pragma config DEBUG = OFF              // Background Debugger Enable (Debugger is disabled)
+#pragma config ICESEL = ICS_PGx2        // ICE/ICD Comm Channel Select (ICE EMUC2/EMUD2 pins shared with PGC2/PGD2)
+#pragma config PWP = OFF                // Program Flash Write Protect (Disable)
+#pragma config BWP = OFF                // Boot Flash Write Protect bit (Protection Disabled)
+#pragma config CP = OFF                 // Code Protect (Protection Disabled)
+
+
+
---
+++ TP/MPLABX_vomnes/FirmwareTP1.X.vomnes-/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=FirmwareTP1.X.vomnes-.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/FirmwareTP1.X.vomnes-.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=firmwaretp1.x.vomnes-.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/firmwaretp1.x.vomnes-.tar
