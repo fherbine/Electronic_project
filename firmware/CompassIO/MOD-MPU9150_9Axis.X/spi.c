ada8058cadf1e6eb86d7c0c961c5ba39baf9cfd0
Valentin Omnes <valentin.omnes@gmail.com>
2018-05-17 20:05:46 +0200

    feat: Add SPI fonctions

--- firmware/CompassIO/MOD-MPU9150_9Axis.X/spi.c
+++ firmware/CompassIO/MOD-MPU9150_9Axis.X/spi.c
@@ -7,7 +7,11 @@
 
 #include "types.h"
 
+#define BITS(X) (1 << X)
+
 #define SPI_BAUD_RATE 42
+
+#define SPIROV BITS(6)
 
 void SPI1_Init()
 {
@@ -18,7 +22,51 @@ void SPI1_Init()
     rData = SPI1BUF;      // Clears the receive buffer
     /* Here to manage interrupts */
 
-    SPI1BRG = SPI_BAUD_RATE;
-    SPI1STATbits.SPIROV = 0;
+    SPI1BRG = SPI_BAUD_RATE; // Set baud rate
+    SPI1STATCLR = SPIROV;    // Clear Receive Overflow Flag Bit
+    SPI1CONbits.MSTEN = 1;   // Enable master mode
+    SPI1CONbits.ON = 1;
+    // SPI1CONbits.SMP ??
+}
 
+u8 SPI1_Read_Data_Ready()
+{
+  return SPI1STATbits.SPIRBF;
+}
+
+
+u8 SPI1_Write_Data_Ready()
+{
+  return SPI1STATbits.SPITBE;
+}
+
+// SDIxpin -> SPIxSR
+u8 SPI1_Read()
+{
+    if (SPI1STATbits.SPIRBF)     // Receive buffer is full - Auto cleared
+    {
+      if (SPI1STATbits.SPIROV)
+        SPI1STATbits.SPIROV = 0; // Overflow has occured
+      return (SPI1BUF & 0xFF);   // 8-bit mode
+    }
+    return (-1);
+}
+
+// SPIxSR -> SDOxpin
+void SPI1_Write(u8 data)
+{
+    if (SPI1STATbits.SPITBE) // SPI Transmit Buffer Full Status bit
+    {
+        SPI1BUF = data;
+    }
+}
+
+void SPI1_Write_String(char *data, unsigned int len)
+{
+  while (len)
+  {
+    SPI1BUF = *data++;
+    while (SPI1STATbits.SPITBF) ; /* Wait SPITBF is cleared */
+    len--;
+  }
 }
