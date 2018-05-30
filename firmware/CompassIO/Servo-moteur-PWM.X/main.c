4c17d91386d25bf580c353df0355098f50244f66
LabElec <labelec@Bocals-iMac-4.local>
2018-05-30 18:17:29 +0200

    feat: Add pwm

--- firmware/CompassIO/Servo-moteur-PWM.X/main.c
+++ firmware/CompassIO/Servo-moteur-PWM.X/main.c
@@ -6,6 +6,8 @@
  */
 
 #include <p32xxxx.h>
+#include "xc.h"
+#include "sys/attribs.h"
 
 #pragma config FPLLIDIV = DIV_2
 #pragma config FPLLMUL = MUL_20
@@ -15,21 +17,63 @@
 #pragma config POSCMOD = HS
 
 #define SYSCLK (8000000/2*20/1) // 80Mhz
-#define PBCLK (SYSCLK/1600)
+#define PBCLK (SYSCLK/8) // 10Mhz
+
 
 /*
  * 
  */
-int main(int argc, char** argv)
-{
-	T3CON = 0;               // 0 on every bit, (timer stop, basic config)
-	TMR3 = 2;                // Clean the timer register
-	IFS0bits.T3IF = 0;       // Clear interrupt status flag
-	T3CONbits.TCKPS = 0b111; // Set scaler 1:256
-	PR3 = PBCLK;    // Setup the period
-	T3CONbits.ON = 1;
 
-	OC1CONbits.OCM = 0b111;
-	OC1RS = (PBCLK*1/20)*1.5;
-	OC1R = (PBCLK*1/20)*1.5;
+void __ISR(_TIMER_2_VECTOR, IPL1) Timer2Handler(void) {
+    TMR2 = 0;
+	LATFbits.LATF1 ^= 1;
+    IFS0bits.T2IF = 0;
+}
+
+void SetDutyCycle(unsigned char dutyCycle)
+{
+	OC1RS = PBCLK/256/5/dutyCycle;
+	OC1R = PBCLK/256/5/dutyCycle;
+}
+
+void Degres(unsigned char degres)
+{
+
+}
+
+int main()
+{
+	__builtin_disable_interrupts();
+	TRISFbits.TRISF1 = 0;
+	LATFbits.LATF1 = 0;
+	T2CON = 0;               // 0 on every bit, (timer stop, basic config)
+	TMR2 = 0;                // Clean the timer register
+	IFS0bits.T2IF = 0;     // Clear interrupt status flag
+	T2CONbits.TCKPS = 0b111; // Set scaler 1:256
+	PR2 = PBCLK/256/50;    // Setup the period > Periph. Bus Clk (set by user) / scaler > sec | 20ms
+   
+    IPC2bits.T2IP = 1; // Set priority
+    IPC2bits.T2IS = 2; // Set subpriority
+    IFS0bits.T2IF = 0; // Clear interrupt status flag
+    IEC0bits.T2IE = 1; // Enable interrupts
+	T2CONbits.ON = 1;
+
+	OC1CONbits.OCM = 0b110;
+	SetDutyCycle(40);
+	OC1CONbits.ON = 1;
+
+	INTCONbits.MVEC = 1; // Enable multi interrupts
+	__builtin_enable_interrupts();
+
+	TRISDbits.TRISD8 = 1;
+	unsigned char count = 40;
+	while(1)
+	{
+		SetDutyCycle(count);
+		if (PORTDbits.RD8 == 0)
+		{
+			count += 1;
+			while (PORTDbits.RD8 == 0);
+		}
+	}
 }
