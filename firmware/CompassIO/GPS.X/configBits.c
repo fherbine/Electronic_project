774b0c0693053f48206554d76e01e4fed123a8c0
fherbine <felix.herbinet@yahoo.com>
2018-05-31 15:05:54 +0200

    feat: Add GPS Codde

---
+++ firmware/CompassIO/GPS.X/configBits.c
@@ -0,0 +1,9 @@
+
+// DEVCFG2
+#pragma config FPLLIDIV = DIV_2         // PLL Input Divider (2x Divider)
+#pragma config FPLLMUL = MUL_20         // PLL Multiplier (20x Multiplier)
+#pragma config FPLLODIV = DIV_1         // System PLL Output Clock Divider (PLL Divide by 1)
+
+// DEVCFG1
+#pragma config FNOSC = PRIPLL           // Oscillator Selection Bits (Primary Osc w/PLL (XT+,HS+,EC+PLL))
+#pragma config POSCMOD = HS             // Primary Oscillator Configuration (HS osc mode)
---
+++ firmware/CompassIO/GPS.X/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: bocal
+ *
+ * Created on May 31, 2018, 2:34 PM
+ */
+
+#include "types.h"
+
+/*
+ * 
+ */
+int main(int argc, char** argv)
+{
+	
+	return (EXIT_SUCCESS);
+}
+
