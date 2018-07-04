5ba2a566bd61f19e4b4caf108817640e106989d3
LabElec <labelec@mac-labelec-02.42.fr>
2018-07-04 11:41:47 +0200

    feat: Add algorithm compass direction

---
+++ firmware/CompassIO/direction.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+
+#define toRad(angle) ((angle) * M_PI / 180)
+#define toDeg(angle) ((angle) / M_PI * 180)
+
+short get_direction(double lat1, double long1, double lat2, double long2)
+{
+  double dLat = toRad(lat2 - lat1);
+  double dLong =  toRad(long2 - long1);
+
+  lat1 = toRad(lat1);
+  lat2 = toRad(lat2);
+
+  double y = sin(dLong) * cos(lat2);
+  double x = cos(lat1) * sin(lat2) - sin(lat1) * cos(lat2) * cos(dLong);
+  double brng = toDeg(atan2(y, x));
+
+  // fix negative degrees
+  if(brng < 0) {
+    brng = 360 - fabs(brng);
+  }
+
+  return brng;
+}
+
+main()
+{
+  printf("%d\n", get_direction(48.89666249816737, 2.3183298110961914, 50.040144614786946, 2.341561138008501));
+}
