cd724463010c364624b39740f79c4101af437778
LabElec <labelec@mac-labelec-02.42.fr>
2018-07-04 12:42:34 +0200

    feat: Start to parse nmea

--- firmware/CompassIO/move.c
+++ firmware/CompassIO/move.c
@@ -4,6 +4,31 @@
 #define toRad(angle) ((angle) * M_PI / 180)
 #define toDeg(angle) ((angle) / M_PI * 180)
 
+unsigned long int		ft_strlen(char *s)
+{
+	char *str = s;
+	while (*s++);
+	return (s-str);
+}
+
+void parse_nmea_gps(char *data)
+{
+  int size = ft_strlen(data);
+  if (size > 6)
+  {
+    if (data[0] == '$' && data[1] == 'G' && data[2] == 'P' && data[3] == 'R' &&
+    data[4] == 'M' && data[5] == 'C')
+    {
+      int i = 0;
+      while (i < size && data[i] != '\n')
+      {
+        printf("%c", data[i]);
+        i++;
+      }
+    }
+  }
+}
+
 short get_direction(double lat1, double long1, double lat2, double long2)
 {
   double dLat = toRad(lat2 - lat1);
@@ -24,9 +49,13 @@ short get_direction(double lat1, double long1, double lat2, double long2)
   return brng;
 }
 
-main()
+int main()
 {
-  printf("%d\n", get_direction(48.89666249816737, 2.3183298110961914, 50.040144614786946, 2.341561138008501));
+  //printf("%d\n", get_direction(48.89666249816737, 2.3183298110961914, 50.040144614786946, 2.341561138008501));
+  parse_nmea_gps("$GPRMC,164933.270,A,4853.7671,N,00219.1216,E,1.74,74.12,010618,,,A*54\n");
+  return (0);
+}
+
 //   printf("%d\n", sizeof(double));
 //
 //
@@ -56,4 +85,3 @@ main()
 //     lon |= tmp;
 //     lon <<= 8;
 //   }
-}
