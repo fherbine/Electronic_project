228c207bd57d151e9f51065b47593b2bc33d0d52
Valentin Omnes <valentin.omnes@gmail.com>
2018-07-18 12:03:18 +0200

    feat: Find a way to store and read float in flash memory

--- firmware/CompassIO/double.c
+++ firmware/CompassIO/double.c
@@ -3,8 +3,18 @@
 
 int main()
 {
-  double data = 123.456;
+  /* Double to long long */
+  double data = 123.999;
   long long value = *(long long *)&data;
   printf("%f - %llx - %llx\n", data, *(long long *)&data, value);
+  /* Merge unsigned char to int */
+  unsigned char array[4] = {0x15, 0xCD, 0x5B, 0x7};
+  unsigned int output = 0;
+  for (int i = 0; i < 4; i++)
+  {
+    output = (array[i] << (i * 8)) | output;
+  }
+  /* Store float * 1000 used as flat by divinding by 1000 */
+  printf("%f\n", output / 1000.0);
   return (0);
 }
