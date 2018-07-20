0bd3c18b4cf5b6d1b2039c8e2e38bb10816b33d5
LabElec42 <fherbine>
2018-07-20 20:56:15 +0200

    feat: Add power on/off sequence

--- firmware/CompassIO/compassIO.X/sources/main.c
+++ firmware/CompassIO/compassIO.X/sources/main.c
@@ -53,14 +53,92 @@ void __ISR(_UART2_VECTOR, IPL2SRS) UART2Handler(void) {
 }
 
 u16 countTime = 0;
-u8 countTimeEnable = FALSE;
+u8  countTimeEnable = FALSE;
 u8  devicePowered = FALSE;
+unsigned char  rst = 0, on_off = 0, gps = 0;
+unsigned short gpsTmp = 0;
+
+u8 powerOnProcess = FALSE;
+u8 powerOffProcess = FALSE;
+
+void	gps_power_on(void)
+{
+	if (gpsTmp == 100 && !rst) // 100ms after PIC power on
+	{
+		LATDbits.LATD6 = 1;
+		rst = 1;
+	}
+	else if (gpsTmp == 110 && !on_off)
+	{
+		LATDbits.LATD5 = 1;
+		on_off = 1;
+	}
+	else if (gpsTmp == 310 && on_off)
+	{
+		LATDbits.LATD5 = 0;
+		on_off = 0;
+	}
+	else if (gpsTmp > 310)
+	{
+		gpsTmp = 0;
+		gps = 1;
+                powerOnProcess = FALSE;
+	}
+}
+
+void	gps_power_off(void)
+{
+	if (gpsTmp == 0)
+		LATDbits.LATD5 = 1;
+	else if (gpsTmp == 200)
+        {
+           	LATDbits.LATD5 = 0;
+                powerOffProcess = FALSE;
+        }
+	gpsTmp++;
+}
+
 #define MAX_U16 0xFFFF
 
-void __ISR(_TIMER_3_VECTOR, IPL1) PowerOff2secHandler(void) {
+void __ISR(_TIMER_3_VECTOR, IPL1) Timer3Handler(void) {
+    /* Power Off 2 Sec */
     if (countTimeEnable && countTime < MAX_U16)
         countTime++;
+    /* ON OFF GPS Sequence */
+    if (powerOnProcess)
+        gps_power_on();
+    if (powerOffProcess)
+        gps_power_off();
+    gpsTmp++;
     IFS0bits.T3IF = 0;
+}
+
+void global_init()
+{
+    __builtin_disable_interrupts();
+    UART1_Init(_8N, 0, UART_RX_TX_ON);
+    UART2_Init(_8N, 0, UART_RX_TX_ON);
+    init_servo();
+    Init_SPI2();
+    INTCONbits.MVEC = 1; // Enable multi interrupts
+    __builtin_enable_interrupts();
+    delayms(1000);
+    ft_putendl("Start");
+    /* GPS */
+    LATDbits.LATD6 = 0; // Set nRST GPS
+    LATDbits.LATD5 = 0; // Set ON_OFF GPS
+    rst = 0;
+    on_off = 0;
+    gps = 0;
+}
+
+void global_off()
+{
+    U1MODEbits.ON = 0; // Disable UART1 Module
+    U2MODEbits.ON = 0; // Disable UART2 Module
+    I2C1CONbits.ON = 0; // Disable I2C1
+    SPI2CONbits.ON = 0; // Disable SPI2 Flash Memory
+    T2CONbits.ON = 0;  // Disable Timer2 Servomotor
 }
 
 #define ONE_HALF_SEC 1500
@@ -69,6 +147,8 @@ void __ISR(_EXTERNAL_1_VECTOR, IPL6) MainButtonHandler(void) {
     if (INTCONbits.INT1EP == 1) { // Button Released
         if (devicePowered && countTime > ONE_HALF_SEC)
         {
+            powerOffProcess = TRUE;
+            gpsTmp = 0;
             ft_putendl("GLOBAL POWER OFF");
             global_off();
             devicePowered = FALSE;
@@ -81,6 +161,8 @@ void __ISR(_EXTERNAL_1_VECTOR, IPL6) MainButtonHandler(void) {
             }
             else
             {
+                powerOnProcess = TRUE;
+                gpsTmp = 0;
                 global_init();
                 ft_putendl("first time = GLOBAL POWER ON");
                 devicePowered = TRUE;
@@ -108,31 +190,13 @@ void init_button()
     IEC0bits.INT1IE = 1;
 }
 
-void global_init()
-{
-    __builtin_disable_interrupts();
-    UART1_Init(_8N, 0, UART_RX_TX_ON);
-    UART2_Init(_8N, 0, UART_RX_TX_ON);
-    init_servo();
-    Init_SPI2();
-    INTCONbits.MVEC = 1; // Enable multi interrupts
-    __builtin_enable_interrupts();
-    delayms(1000);
-    ft_putendl("Start");
-}
-
-void global_off()
-{
-    U1MODEbits.ON = 0; // Disable UART1 Module
-    U2MODEbits.ON = 0; // Disable UART2 Module
-    I2C1CONbits.ON = 0; // Disable I2C1
-    SPI2CONbits.ON = 0; // Disable SPI2 Flash Memory
-    T2CONbits.ON = 0;  // Disable Timer2 Servomotor
-}
-
 void main()
 {
     LATFbits.LATF1 = 0;
+    TRISDbits.TRISD6 = 0; // RD6 is an output -> nRST GPS
+    LATDbits.LATD6 = 1;
+    TRISDbits.TRISD5 = 0; // RD5 is an output -> ON_OFF GPS
+    
     __builtin_disable_interrupts();
     Init_Delay();
     init_button();
@@ -143,6 +207,7 @@ void main()
 }
 
 /*
+ *
      delayms(100);
     store_double(0x030000, 999);
     delayms(85);
